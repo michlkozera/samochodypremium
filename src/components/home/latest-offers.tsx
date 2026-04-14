ac784355747169ac0194332f257aece41780097a
michlkozera <michal.kozera.01@gmail.com>
2026-04-14 14:05:59 +0200

    v30

--- src/components/home/latest-offers.tsx
+++ src/components/home/latest-offers.tsx
@@ -43,11 +43,13 @@ export function LatestOffers({ vehicles }: LatestOffersProps) {
           </div>
         </MotionReveal>
 
-        {/* Grid - 5 columns on large screens */}
+        {/* Vertical stack - wider cards */}
         <MotionReveal stagger={0.08} delay={0.1}>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 lg:gap-5">
+          <div className="flex flex-col gap-4 lg:gap-5">
             {latestVehicles.map((vehicle) => (
-              <VehicleCard key={vehicle.id} vehicle={vehicle} />
+              <div key={vehicle.id} className="w-full max-w-none">
+                <VehicleCard vehicle={vehicle} />
+              </div>
             ))}
           </div>
         </MotionReveal>
